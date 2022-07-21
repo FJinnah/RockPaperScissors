ed725966559fd39db5d93745feb31e724bdb1686
Farzeen Jinnah <farzeenj@gmail.com>
2022-07-21 17:52:17 -0600

    Add user prompt and all possible choices for game
    Added user prompt for selection of rock, paper, or scissors.
    Still need to add case insensitivity. Also added if, else blocks for
    the rest of the possible game permutations in the playRound function.

--- script.js
+++ script.js
@@ -9,7 +9,20 @@ function getComputerChoice(){
     }
 }
 
-function playRound (playerSelection, computerSelection) {
+let getPlayerChoice = prompt ('Please type in either rock, paper, or scissors.');
+
+if (getPlayerChoice == 'rock') {
+    getPlayerChoice = 'rock';
+} else if (getPlayerChoice == 'scissors') {
+    getPlayerChoice = 'scissors';
+} else if (getPlayerChoice == 'paper') {
+    getPlayerChoice = 'paper';
+} else {
+    alert('Choice not recognized. Please refresh the page.')
+}
+
+
+function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock') {
         if (computerSelection === 'scissors') {
             return 'Rock wins!';
@@ -19,9 +32,49 @@ function playRound (playerSelection, computerSelection) {
             return 'Tie game!';
         }
         }
+    
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'Scissors wins!';
+        } else if (computerSelection === 'rock') {
+            return 'Paper wins!';
+        } else {
+            return 'Tie game!';
+        }
+        }
+
+        if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return 'Scissors wins!';
+        } else if (computerSelection === 'rock') {
+            return 'Rock wins!';
+        } else {
+            return 'Tie game!';
+        }
+        }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection); {
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'scissors') {
+            return 'Rock wins!';
+        } else if (computerSelection === 'paper') {
+            return 'Paper wins!';
+        } else {
+            return 'Tie game!';
+        }
+        }
+    }
+    console.log(playRound(playerSelection, computerSelection))
+}
 }
 
 
-const playerSelection = 'rock';
+   
+
+
+const playerSelection = getPlayerChoice;
 const computerSelection = getComputerChoice();
-console.log (playRound(playerSelection, computerSelection));
+console.log(game());
