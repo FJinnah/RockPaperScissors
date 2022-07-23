17f691a404a91f40fcea259bc4203775253d5d10
Farzeen Jinnah <farzeenj@gmail.com>
2022-07-22 20:15:02 -0600

    Add functions inside for loop.

--- script.js
+++ script.js
@@ -64,11 +64,42 @@ let tieMessage = 'This round is a tie';
 
 function game() {
     for (let i = 0; i < 5; i++) {
-      playRound();
+    
+    function getComputerChoice() {
+    let choice = Math.random() * 3;
+    if (choice <= 1) {
+        return 'rock';
+    } else if (choice <=2) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
 }
+
+let getPlayerChoice = prompt ('Please type in either rock, paper, or scissors.').toLowerCase();
+
+if (getPlayerChoice == 'rock') {
+    getPlayerChoice = 'rock';
+} else if (getPlayerChoice == 'scissors') {
+    getPlayerChoice = 'scissors';
+} else if (getPlayerChoice == 'paper') {
+    getPlayerChoice = 'paper';
+} else {
+    alert('Choice not recognized. Please refresh the page.')
 }
+    let roundResult = playRound (playerSelection, computerSelection);
+    console.log(roundResult);
+
+    if (roundResult = playerWinMessage) {
+        console.log('1 point for player!');
+    } else {
+        console.log('1 point for computer!');
+    }
+    }
+}      
+
 
 const playerSelection = getPlayerChoice;
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
