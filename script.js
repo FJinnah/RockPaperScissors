6a604d662a791903770fd30520f1541c34bf5e46
Farzeen Jinnah <farzeenj@gmail.com>
2022-09-15 08:08:18 -0600

    Change innerHTML to innerText for security

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const computerSelection = getComputerChoice();
 
 //Allows the computer to randomly choose between rock, paper, and scissors.
 function getComputerChoice() {
-    for (let i = 0; i <20; i++) {
     let choice = Math.random() * 3;
     if (choice <= 1) {
         return 'rock';
@@ -17,31 +16,34 @@ function getComputerChoice() {
         return 'scissors';
     }
     }
-}
 
 //Increments the player score
 function playerWin() {
     let playerTestScore = document.querySelector('#playerScore');
-    let number = playerTestScore.innerHTML;
-    number++;
-    playerTestScore.innerHTML = number;
+    let number1 = playerTestScore.innerText;
+    number1++;
+    playerTestScore.innerText = number1;
+    playerScore++;
 }
 
 //Increments the computer score
 function computerWin() {
     let computerTestScore = document.querySelector('#computerScore');
-    let number = computerTestScore.innerHTML;
-    number++;
-    computerTestScore.innerHTML = number;
+    let number2 = computerTestScore.innerText;
+    number2++;
+    computerTestScore.innerText = number2;
+    computerScore++;
 }
 
 //Increments the tie score
 function tieWin() {
     let tiesTestScore = document.querySelector('#tiesScore');
-    let number = tiesTestScore.innerHTML;
-    number++;
-    tiesTestScore.innerHTML = number;
+    let number3 = tiesTestScore.innerText;
+    number3++;
+    tiesTestScore.innerText = number3;
+    ties++;
 }
+
 
 //Allows the player to choose between rock, paper, and scissors. They are prompted in the game() function.
 function getPlayerChoice() {}
@@ -78,50 +80,33 @@ function playRound(playerSelection, computerSelection) {
         }
         }
     }
-//Combines everything into a proper game. The loop is run and ends when either the player or computer reaches 3 (best out of 5).
-// function game() {
-//     for (let i = 0; i < 20; i++) {
-//         if (playerScore == 3) {
-//             return console.log('Player wins!');
-//         } else if (computerScore == 3) {
-//             return console.log('Computer wins :(');
-//         }
-//         const computerSelection = getComputerChoice();
-//         const playerSelection = document.querySelector('.button');
-//         // prompt ('Please type in either rock, paper, or scissors.').toLowerCase();
-//         console.log(playRound(playerSelection, computerSelection));
-// }      
-// }
 
 //UI additions
 const rockChoice = document.querySelector('#rockChoice');
 rockChoice.addEventListener('click', () => {
     const rockResult = playRound('rock', getComputerChoice());
     const scoreOne = document.querySelector('#score');
-    // const scoreDisplay = document.createElement('p');
-    // scoreDisplay.textContent = rockResult.toString();
-    // scoreOne.appendChild(scoreDisplay);
 });
 
 const paperChoice = document.querySelector('#paperChoice');
 paperChoice.addEventListener('click', () => {
     const paperResult = playRound('paper', getComputerChoice());
     const scoreOne = document.querySelector('#score');
-    // const scoreDisplay = document.createElement('p');
-    // scoreDisplay.textContent = paperResult.toString();
-    // scoreOne.appendChild(scoreDisplay);
 });
 
 const scissorsChoice = document.querySelector('#scissorsChoice');
-scissorsChoice.addEventListener('click', () => {
+scissorsChoice.addEventListener('click', () => {score
     const scissorsResult = playRound('scissors', getComputerChoice());
     const scoreOne = document.querySelector('#score');
-    // const scoreDisplay = document.createElement('p');
-    // scoreDisplay.textContent = scissorsResult.toString();
-    // scoreOne.appendChild(scoreDisplay);
-    
-    // playRound('scissors', computerSelection)
 });
+
+// function fullGame() {
+    
+// }
+
+
+// End Game Function: Add .disabled for each button
+
 
 // //Runs the function in the console.
 // game();
